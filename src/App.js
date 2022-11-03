1c34cd14263168eb45618d4e7c49f23ddfbf1f2a
Arun <72662617+Arun-1903@users.noreply.github.com>
2022-11-04 01:21:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Cards from "./components/Cards"
+import {db,ref,set} from "./firebase"
 import './styles/main.css';
 
-function App() {8
+
+function App() {
+  const change=()=>{
+    set(ref(db, 'values/clicked' ), true);
+    alert("Irrigating Now")
+    setTimeout(()=>{
+      set(ref(db, 'values/clicked' ), false);
+      alert("Irrigation finished")
+    },3000)
+  }
+  
   return (
 <div className="app">
       <img className='image' src="plant.png" alt="" />
       <Cards/>
+      <button className="btn" onClick={change}>Irrigate</button>
       </div> 
   );
 }
